38b0c68dfe8fdf931a96f55350d9b96f85738ec7
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-12-12 10:38:59 +0100

    make dist

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,12 +5,9 @@ declare(strict_types=1);
 $configurator = require __DIR__ .'/vendor/sigwin/infra/resources/PHP/php-cs-fixer.php';
 
 $header = <<<'EOF'
-This file is part of the Sigwin Infra project.
+This file is part of the dkarlovi.github.io project.
 
 (c) sigwin.hr
-
-This source file is subject to the MIT license that is bundled
-with this source code in the file LICENSE.
 EOF;
 
 $config = $configurator(__DIR__, $header);
