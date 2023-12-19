50946200a1a0af69bd649f1f234e810349e53c42
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-12-19 11:04:16 +0100

    Relative date

--- assets/app.ts
+++ assets/app.ts
@@ -1,5 +1,15 @@
 import './styles/app.scss';
 
+document.addEventListener('DOMContentLoaded', function() {
+    const dateContainers = document.querySelectorAll('.date-container');
+    dateContainers.forEach(function(container) {
+        const days = (Date.now() - new Date(container.title).getTime()) / 86400000;
+        const unit = [{unit: 'year', threshold: 365}, {unit: 'month', threshold: 30}, {unit: 'week', threshold: 7}, {unit: 'day', threshold: 1}].find(unit => days >= unit.threshold);
+        const relativeTimeValue= -1 * Math.floor(days / unit.threshold);
+        container.innerText = new Intl.RelativeTimeFormat('en', { numeric: 'auto' }).format(relativeTimeValue, unit.unit);
+    });
+});
+
 import Collapse from "bootstrap/js/src/collapse";
 import Dropdown from "bootstrap/js/src/dropdown";
 import Modal from "bootstrap/js/src/modal";
