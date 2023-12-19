44d79ee5e16151fee4f94adbe06a46e431d49fd9
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-12-19 17:10:17 +0100

    fix: enable dropdown for mobile menu

--- assets/app.ts
+++ assets/app.ts
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // import Collapse from "bootstrap/js/src/collapse";
-// import Dropdown from "bootstrap/js/src/dropdown";
+import Dropdown from "bootstrap/js/src/dropdown";
 // import Modal from "bootstrap/js/src/modal";
 import Offcanvas from "bootstrap/js/src/offcanvas";
 // import Tab from "bootstrap/js/src/tab";
